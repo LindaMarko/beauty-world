0345bcf95bd9152b9619bcf67723dedaafcd39c7
Linda Marko <lindamarko83@gmail.com>
2022-08-03 19:39:08 +0200

    Add brand store controller - backend

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Image;
 
 class BrandController extends Controller
 {
@@ -14,8 +15,31 @@ class BrandController extends Controller
         return view('backend.brand.brand_view', compact('brands'));
     }
 
-    public function BrandStore()
+    public function BrandStore(Request $request)
     {
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_sv' => 'required',
+            'brand_image' => 'required',
+        ],
+        [
+            'brand_name_en.required' => 'This input field is required.',
+            'brand_name_sv.required' => 'This input field is required.'
+        ]);
 
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url = 'upload/brand/'.$name_gen;
+
+        Brand::insert([
+            'brand_name_en' => $request->brand_name_en,
+            'brand_name_sv' => $request->brand_name_sv,
+            'brand_slug_en' => strtolower(str_replace('','-',$request->brand_name_en)),
+            'brand_slug_sv' => strtolower(str_replace('','-',$request->brand_name_sv)),
+            'brand_image' => $save_url,
+        ]);
+
+        return redirect()->back();
     }
 }
--- resources/views/backend/brand/brand_view.blade.php
+++ resources/views/backend/brand/brand_view.blade.php
@@ -65,6 +65,9 @@
               <h5>Brand Name English <span class="text-danger">*</span></h5>
               <div class="controls">
                 <input type="text" name="brand_name_en" class="form-control">
+                @error('brand_name_en')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
               </div>
             </div>
 
@@ -72,18 +75,24 @@
               <h5>Brand Name Swedish <span class="text-danger">*</span></h5>
               <div class="controls">
                 <input type="text" name="brand_name_sv" class="form-control">
+                @error('brand_name_sv')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
               </div>
             </div>
 
             <div class="form-group">
               <h5>Brand Image <span class="text-danger">*</span></h5>
               <div class="controls">
-                <input type="file" name="brand_image" class="form-control" required >
+                <input type="file" name="brand_image" class="form-control">
+                @error('brand_image')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
               </div>
             </div>
 
             <div class="text-xs-right">
-              <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add">
+              <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add New Brand">
             </div>
            </form>
          </div>
