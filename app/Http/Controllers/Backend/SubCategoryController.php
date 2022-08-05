2ceca6d67e766a50077ec7821d687bb5ac224264
Linda Marko <lindamarko83@gmail.com>
2022-08-05 19:43:33 +0200

    Add Sub SubCategory CRUD - backend

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\SubCategory;
+use App\Models\SubSubCategory;
 
 use Illuminate\Http\Request;
 
@@ -68,6 +69,80 @@ class SubCategoryController extends Controller
     public function SubCategoryDelete($id){
 
     	SubCategory::findOrFail($id)->delete();
+		return redirect()->back();
+
+    }
+
+
+    /////////////// That for SUB->SUBCATEGORY ////////////////
+
+    public function SubSubCategoryView()
+    {
+        $categories = Category::orderBy('category_name_en','ASC')->get();
+        $subsubcategory = SubSubCategory::latest()->get();
+        return view('backend.category.sub_subcategory_view',compact('subsubcategory','categories'));
+
+    }
+
+    public function GetSubCategory($category_id){
+
+        $subcat = SubCategory::where('category_id',$category_id)->orderBy('subcategory_name_en','ASC')->get();
+        return json_encode($subcat);
+    }
+
+    public function SubSubCategoryStore(Request $request){
+
+        $request->validate([
+             'category_id' => 'required',
+             'subcategory_id' => 'required',
+             'subsubcategory_name_en' => 'required',
+             'subsubcategory_name_sv' => 'required',
+         ],[
+             'category_id.required' => 'Please select Any option',
+             'subsubcategory_name_en.required' => 'Input SubSubCategory English Name',
+         ]);
+
+        SubSubCategory::insert([
+         'category_id' => $request->category_id,
+         'subcategory_id' => $request->subcategory_id,
+         'subsubcategory_name_en' => $request->subsubcategory_name_en,
+         'subsubcategory_name_sv' => $request->subsubcategory_name_sv,
+         'subsubcategory_slug_en' => strtolower(str_replace(' ', '-',$request->subsubcategory_name_en)),
+         'subsubcategory_slug_sv' => strtolower(str_replace(' ', '-',$request->subsubcategory_name_sv)),
+         ]);
+
+         return redirect()->back();
+
+     }
+
+     public function SubSubCategoryEdit($id){
+    	$categories = Category::orderBy('category_name_en','ASC')->get();
+    	$subcategories = SubCategory::orderBy('subcategory_name_en','ASC')->get();
+    	$subsubcategories = SubSubCategory::findOrFail($id);
+    	return view('backend.category.sub_subcategory_edit',compact('categories','subcategories','subsubcategories'));
+
+    }
+
+    public function SubSubCategoryUpdate(Request $request){
+
+    	$subsubcat_id = $request->id;
+
+    	SubSubCategory::findOrFail($subsubcat_id)->update([
+		'category_id' => $request->category_id,
+		'subcategory_id' => $request->subcategory_id,
+		'subsubcategory_name_en' => $request->subsubcategory_name_en,
+		'subsubcategory_name_sv' => $request->subsubcategory_name_sv,
+		'subsubcategory_slug_en' => strtolower(str_replace(' ', '-',$request->subsubcategory_name_en)),
+		'subsubcategory_slug_sv' => strtolower(str_replace(' ', '-',$request->subsubcategory_name_sv)),
+    	]);
+
+		return redirect()->route('all.subsubcategory');
+
+    }
+
+    public function SubSubCategoryDelete($id){
+
+    	SubSubCategory::findOrFail($id)->delete();
 		return redirect()->back();
 
     }
--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,14 @@ Route::prefix('category')->group(function(){
     Route::post('sub/update', [SubCategoryController::class, 'SubCategoryUpdate'])->name('subcategory.update');
     Route::get('sub/delete/{id}', [SubCategoryController::class, 'SubCategoryDelete'])->name('subcategory.delete');
 
+    // Admin Sub->Sub Category All Routes
+    Route::get('/sub/sub/view', [SubCategoryController::class, 'SubSubCategoryView'])->name('all.subsubcategory');
+    Route::get('/subcategory/ajax/{category_id}', [SubCategoryController::class, 'GetSubCategory']);
+    Route::post('/sub/sub/store', [SubCategoryController::class, 'SubSubCategoryStore'])->name('subsubcategory.store');
+    Route::get('/sub/sub/edit/{id}', [SubCategoryController::class, 'SubSubCategoryEdit'])->name('subsubcategory.edit');
+    Route::post('/sub/update', [SubCategoryController::class, 'SubSubCategoryUpdate'])->name('subsubcategory.update');
+    Route::get('/sub/sub/delete/{id}', [SubCategoryController::class, 'SubSubCategoryDelete'])->name('subsubcategory.delete');
+
 });
 
 
