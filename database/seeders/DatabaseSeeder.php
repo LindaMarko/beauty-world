b7d9a0368e34d0e4b5f4d2b3c656a5aa6f038c1f
Linda Marko <lindamarko83@gmail.com>
2022-08-20 16:03:14 +0200

    Change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Admin::factory()->create();
+        // \App\Models\Admin::factory()->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
